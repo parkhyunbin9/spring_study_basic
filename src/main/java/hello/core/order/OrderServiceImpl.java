095a11f46f15f7b8cb407d582058b2ddaa19059b
parkhyunbin9 <parkhyunbin9@gmail.com>
2022-05-26 02:55:49 +0900

    - @ComponentScan - Lombok, @Primary, @Qualifier - 다양한 방법의 Autowired

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,20 +4,50 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
+//  @Autowired  오래된 방식 테스트에 어려움, 안쓰는 것이 좋다.DI 프레임워크 없이 할 수 없다.(순수한 테스트 생성 불가능) 단, 테스트 코드, Configuration 에서는 사용 가능
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
-
+    @Autowired    //생성자가 하나이므로 @Autowired 생랴 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+/*
+
+
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Autowired(required = false) // 선택적
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
